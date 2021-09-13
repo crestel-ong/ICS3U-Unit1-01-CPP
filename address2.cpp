41ea12dd5d925f3a0b2000b4258392957fbcf26a
crestel-ong <crestel.ong@mths.ca>
2021-09-13 20:39:08 +0000

    proper style and right directory

--- address2.cpp
+++ address2.cpp
@@ -1,7 +1,13 @@
+// Copyright (c) 2021 Crestel Ong All rights reserved
+//
+// Created by: Crestel Ong
+// Created on: Sept 2021
+// This is the "address" program, with proper style
+
 #include <iostream>
 
 int main() {
-	std::cout << "Crestel Ong" << std::endl;
-	std::cout << "Ontario" << std::endl;
-	std::cout << "Canada" << std::endl;
+    std::cout << "Crestel Ong" << std::endl;
+    std::cout << "Ontario" << std::endl;
+    std::cout << "Canada" << std::endl;
 }
